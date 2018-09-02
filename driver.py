075bd03bfa17bc7acbd9779ae5705274a052bf34
Arjun43 <42911423+Arjun43@users.noreply.github.com>
2018-09-02 19:02:47 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -14,10 +14,24 @@ def read_directory(mypath):
 
 # Function you will be working with
 def create_knowledge_graph(contents_of_input_file, name_of_input_file):
-    # Through this function you have to use the contents of each file to create a knowledge graph
-    # The output has to be saved in the data/output folder with the same name as data/input file
-    # Note the writing to file has to be handled by you.
-    pass
+    #Import Library
+#Import other necessary libraries like pandas, numpy...
+from sklearn import linear_model
+#Load Train and Test datasets
+#Identify feature and response variable(s) and values must be numeric and numpy arrays
+x_train=input_variables_values_training_datasets
+y_train=target_variables_values_training_datasets
+x_test=input_variables_values_test_datasets
+# Create linear regression object
+linear = linear_model.LinearRegression()
+# Train the model using the training sets and check score
+linear.fit(x_train, y_train)
+linear.score(x_train, y_train)
+#Equation coefficient and Intercept
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
+#Predict Output
+predicted= linear.predict(x_test)
 
 #Main function
 if __name__ == '__main__':
